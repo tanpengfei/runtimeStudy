b959b3e8dc94f4a953d570b94d805e9466f421a7
plyn <tpfair@163.com>
2014-12-26 11:11:15 +0800

    初建
    用于学习

---
+++ RunTime_Study/RunTime_Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTime_Study
+//
+//  Created by plyn on 14/12/26.
+//  Copyright (c) 2014年 lifesense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunTime_Study/RunTime_Study/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunTime_Study
+//
+//  Created by plyn on 14/12/26.
+//  Copyright (c) 2014年 lifesense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
